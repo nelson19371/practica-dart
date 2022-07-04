0b15507ced5aed9a7b9e54cb75160872a4dc8236
@nelsondev <50004176+nelson19371@users.noreply.github.com>
2022-07-04 12:15:00 -0400

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -67,5 +67,65 @@ void main(List<String> arguments) {
   listaString.addAll(['Javier', 'de los angeles']);
   
   print(listaString);
+ 
+ //LISTAS AVANZADAS
+  
+  var alumnos = ['Israel','Carolina','Sergio'];
+  
+  alumnos.forEach((alumno){
+    print(alumno);
+  });
+  
+  print('-------');
+  
+ 
+  //Propiedades de array (se diferencian de las funciones al no tener parentesis)
+ 
+  //Muestra la lista a la inversa con la propiedad reversed
+  print(alumnos.reversed);
+  
+  
+  //Muestra el primer objeto de la lista de alumnos
+  print(alumnos.first);
+  
+  //Muestra el ultimo objeto de la lista de alumnos
+  print(alumnos.last);
+  
+  print('--------');
+  
+  
+  //Funcion de lista que inserta un objeto en el espacio indicado por el primer digito
+  alumnos.insert(2,'Nelson');
+  print(alumnos);
+  
+  //Funcion de lista para insertar mas de un objeto en la lista indicando la posicion inicial
+  alumnos.insertAll(1,['Sarita','Nancy']);
+  print(alumnos);
+  print('-------');
+  
+  //Funcion para remover un objeto de la lista indicando la posicion en la lista
+  alumnos.removeAt(3);
+  print(alumnos);
+  
+  //Funcion para remover un objeto de la lista indicando el valor
+  alumnos.remove('Nelson');
+  print(alumnos);
+  
+  //Funcion para remover un objeto de la lista indicando una funcion
+  alumnos.removeWhere((alumno)=> alumno =='Sergio');
+  print(alumnos);
+   print('-------');
+  
+  //El tipo de datos Map se utiliza para almacenar pares de clave/valor
+  print(alumnos.asMap());
+  
+  //Juntar los datos y separarlos por el parametro a ingresar
+  print(alumnos.join(','));
+  
+  //Filtrar Datos
+  List alumnosConS = alumnos.where((alumno)=> alumno.startsWith('S')).toList();
+  
+  print(alumnosConS);
+  
   
 }

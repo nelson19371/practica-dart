c7be6c629f2d580907242a23447e26766d371d42
@nelsondev <50004176+nelson19371@users.noreply.github.com>
2022-07-04 09:53:11 -0400

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -2,16 +2,21 @@
 //     as dart_application_1;
 
 void main(List<String> arguments) {
-  String correo;
+   String correo;
   String mensaje;
 
   correo = 'israel@gmail.com';
 
+  // Metodos de string
+
   print(correo.contains('@'));
   print(correo.endsWith('.com'));
 
-  mensaje = correo.contains('@')
+  //Validacion de correo electronico, mientras correo contenga @ y termine con '.com'
+
+  mensaje = correo.contains('@') && correo.endsWith('.com')
       ? 'Es un correo electronico'
       : 'No es un correo electronico';
   print(mensaje);
+  
 }
